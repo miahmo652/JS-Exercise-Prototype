7ecc1fdd4b8c3ffd25cc3632e81c17ad2bd47235
miahmo652 <miahmo652@users.noreply.github.com>
2020-03-17 20:33:20 -0400

    complete

--- index.js
+++ index.js
@@ -9,14 +9,14 @@
 
 // EXAMPLE SOLUTION CODE:
 function Airplane(name) {
-  this.name = name;
-  this.isFlying = false;
+    this.name = name;
+    this.isFlying = false;
 }
-Airplane.prototype.takeOff = function () {
-  this.isFlying = true;
+Airplane.prototype.takeOff = function() {
+    this.isFlying = true;
 };
-Airplane.prototype.land = function () {
-  this.isFlying = false;
+Airplane.prototype.land = function() {
+    this.isFlying = false;
 };
 
 
@@ -39,47 +39,71 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+}
+Person.prototype.eat = function(someFood) {
+    if (this.stomach.length < 10) { this.stomach.push(someFood) }
+}
+Person.prototype.poop = function() {
+    this.stomach.splice(0, 10);
+}
+Person.prototype.toString = function() {
+        return this.name + this.age;
+    }
+    /*
+      TASK 2
+        - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
+        - All instances built with Car:
+            + should initialize with an `tank` at 0
+            + should initialize with an `odometer` at 0
+        - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+        - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
+            + Should cause the `odometer` to go up.
+            + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+        - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
+            + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
+    */
+
+function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
 
 }
-
-/*
-  TASK 2
-    - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
-    - All instances built with Car:
-        + should initialize with an `tank` at 0
-        + should initialize with an `odometer` at 0
-    - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
-    - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
-        + Should cause the `odometer` to go up.
-        + Should cause the the `tank` to go down taking `milesPerGallon` into account.
-    - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
-        + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
-*/
-
-function Car() {
-
+Car.prototype.fill = function(gallons) {
+        return this.tank += gallons;
+    }
+    /*
+      TASK 3
+        - Write a Baby constructor subclassing Person.
+        - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
+        - Besides the methods on Person.prototype, babies have the ability to `.play()`:
+            + Should return a string "Playing with x", x being the favorite toy.
+    */
+function Baby(name, age, favoriteToy) {
+    this.name = name;
+    this.age = age;
+    this.favoriteToy = favoriteToy;
+    Person.call(this, name, age);
 }
-
-/*
-  TASK 3
-    - Write a Baby constructor subclassing Person.
-    - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
-    - Besides the methods on Person.prototype, babies have the ability to `.play()`:
-        + Should return a string "Playing with x", x being the favorite toy.
-*/
-function Baby() {
-
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function() {
+    return "Playing with " +
+        this.favoriteToy;
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. when in global scope "this" will be referring to the object it is in.
+  2. The object that is standing before the dot is what this keyword will be bound to
+  3. In a constructor function "this" is referring to the instance that is created.
+  4. this is explicitly defined when using call or apply.
 */
 
 
@@ -87,9 +111,9 @@ function Baby() {
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
 if (typeof exports !== 'undefined') {
-  module.exports = module.exports || {}
-  if (Airplane) { module.exports.Airplane = Airplane }
-  if (Person) { module.exports.Person = Person }
-  if (Car) { module.exports.Car = Car }
-  if (Baby) { module.exports.Baby = Baby }
+    module.exports = module.exports || {}
+    if (Airplane) { module.exports.Airplane = Airplane }
+    if (Person) { module.exports.Person = Person }
+    if (Car) { module.exports.Car = Car }
+    if (Baby) { module.exports.Baby = Baby }
 }
